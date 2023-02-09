294b1ea6ee1632d582bdd0de4dcd78b817cdf4ec
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2023-02-09 20:06:47 +0100

    Create getInstalledDependencies method

--- src/services/RegistryService/types.ts
+++ src/services/RegistryService/types.ts
@@ -7,3 +7,14 @@ export interface RegistryData {
 export interface RegistryServiceStructure extends ServiceStructure {
   getRegistry: (packageName: string, version: string) => Promise<RegistryData>;
 }
+
+export interface Dependency {
+  packageName: string;
+  packageVersion: string;
+  typesVersion: string;
+  isInstalled: boolean;
+  hasTypes: boolean;
+  hasTypesInstalled: boolean;
+}
+
+export type Dependecies = Dependency[];
